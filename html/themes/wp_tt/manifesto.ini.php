619da5a439d587e349dd25324b696a7e350d0cb3
bis5 <bis5@bis5.mydns.jp>
2013-02-05 22:06:20 +0900

    first commit

---
+++ html/themes/wp_tt/manifesto.ini.php
@@ -0,0 +1,20 @@
+<?php
+/**
+
+[Manifesto]
+Name="wp_Twenty_Twelve"
+Depends=Legacy_RenderSystem,legacy
+Url="http://blog.bis5.net/xoops/"
+Version="1.00"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XOOPS2 Legacy Style"
+Author="bis5"
+ScreenShot="screenshot.png"
+Description="WordPress 3.5.x default theme 'Twenty Twelve' clone"
+
+Licence="GPLv2"
+
+*/
+?>
---
+++ html/themes/wp_tt/xugj_menu_lang.php
@@ -0,0 +1,2 @@
+<?php
+define('_WPTT_TOP_MENU', '<li><a href="'.XOOPS_URL.'">Top</a></li>');
